f2a382ab811e021a0a0914ab7cc653b2f1d13aa2
frongerio <ervimoz05@gmail.com>
2025-09-12 16:48:41 +0300

    PRADŽIA

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main() {
+    std::string text;
+
+    std::cout << "Pasirinkite ką norėtumete hashuoti:" << std::endl;
+    std::cout << "1 - Įrašytą tekstą" << std::endl;
+    std::cout << "2 - .txt failą" << std::endl;
+    
+    int choice;
+    bool patikra = false;
+
+    while (!patikra){
+        std::cout<< "Jūsų pasirinkimas: ";
+        std::cin >> choice;
+        if(choice < 1 || choice > 2){
+            std::cout << "Klaida! Įveskite skaičių." << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+        else {
+            patikra = true;
+        }
+    }
+
+    switch(choice){
+        case 1:
+        stfd::cout << "Įveskite teksto kombinaciją, kuria norite užhashinti: ";
+    }
+    return 0;
+
+}
