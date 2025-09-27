ce07e3323123b85ef5adecdbb98d2d7392c7a436
frongerio <ervimoz05@gmail.com>
2025-09-27 15:29:28 +0300

    main.cpp pataisymai

--- main.cpp
+++ main.cpp
@@ -2,12 +2,15 @@
     
 
 int main() {
-
-    std::cout << std::endl;
     
-    std::cout << "Sveiki atvykę į paprastą hash funkcijos generatorių!" << std::endl;
+    bool kartoti = true;
     
-    std::cout << std::endl;
+    while(kartoti) {
+        std::cout << std::endl;
+        
+        std::cout << "Sveiki atvykę į paprastą hash funkcijos generatorių!" << std::endl;
+        
+        std::cout << std::endl;
 
     std::cout << "Pasirinkite ką norėtumete hashuoti:" << std::endl;
     std::cout << "1 - Įrašytą tekstą" << std::endl;
@@ -86,19 +89,23 @@ int main() {
         break;
     }
 
+    auto start = std::chrono::high_resolution_clock::now();
     std::string hash = HashFunkcija(tekstas);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    std::cout << "Hash funkcijos generavimo laikas: " << duration.count() << " sek." << std::endl;
     std::cout << hash << std::endl;
 
     std::cout << std::endl;
 
-    std::cout << "Ar norėtumėte patikrinti koalizijų atsiradimą? (1 - Taip, 2 - Ne): ";
+    std::cout << "Ar norėtumėte atlikti papildomus testus (koalizijos, lavinos efekto, negrįžtamumo)? (1 - Taip, 2 - Ne): ";
     
-    int koalicijosPasirinkimas;
+    int papildomiTestai;
     bool patikrinimas = false;
 
     while(!patikrinimas){
-        std::cin >> koalicijosPasirinkimas;
-        if(koalicijosPasirinkimas < 1 || koalicijosPasirinkimas > 2){
+        std::cin >> papildomiTestai;
+        if(papildomiTestai < 1 || papildomiTestai > 2){
             std::cout << "Klaida! Įveskite skaičių." << std::endl;
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
@@ -108,74 +115,113 @@ int main() {
         }
     }
 
-    switch(koalicijosPasirinkimas){
-        case 1:
-            {
-            int length;
-            std::cout << "Įveskite simbolių kiekį, kurį norėtumėte sugeneruoti: ";
-            std::cin >> length;
-            KoalizijosPatikra(length);
-            }
-            break;
-        case 2:
-            std::cout << "Programa baigė darbą." << std::endl;
-            break;
-    }
+    if(papildomiTestai == 1) {
+        std::cout << "Ar norėtumėte patikrinti koalizijų atsiradimą? (1 - Taip, 2 - Ne): ";
+        
+        int koalicijosPasirinkimas;
+        bool patikrinimas2 = false;
 
-    std::cout << "Ar norėtumėte atlikti lavinos efekto testą (100k porų)? (1 - Taip, 2 - Ne): ";
-    int lavinosPasirinkimas;
-    bool patikrinimas2 = false;
-    while(!patikrinimas2){
-        std::cin >> lavinosPasirinkimas;
-        if(lavinosPasirinkimas < 1 || lavinosPasirinkimas > 2){
-            std::cout << "Klaida! Įveskite skaičių." << std::endl;
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        }
-        else {
-            patikrinimas2 = true;
-        }
-    }
-    switch(lavinosPasirinkimas){
-        case 1:
-            TestuokLavinosEfekta(HashFunkcija);
-            break;
-        case 2:
-            std::cout << "Testas atsisakytas." << std::endl;
-            break;
-    }
-    std::cout << "Ar noretumete atlikti negriztamumo demonstracija? (1 - Taip, 2 - Ne): ";
-    int negriztamumoPasirinkimas;
-    bool patikrinimas3 = false;
-    while(!patikrinimas3){
-        std::cin >> negriztamumoPasirinkimas;
-        if(negriztamumoPasirinkimas < 1 || negriztamumoPasirinkimas > 2){
-            std::cout << "Klaida! Įveskite skaičių." << std::endl;
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        }
-        else {
-            patikrinimas3 = true;
-        }
-    }
-    switch(negriztamumoPasirinkimas){
-        case 1:
-            {
-            std::string inputas;
-            std::cout << "Įveskite norimą tekstą: ";
-            std::cin >> inputas;
-            TestHiding(inputas);
-            TestPuzzleFriendliness(inputas);
+        while(!patikrinimas2){
+            std::cin >> koalicijosPasirinkimas;
+            if(koalicijosPasirinkimas < 1 || koalicijosPasirinkimas > 2){
+                std::cout << "Klaida! Įveskite skaičių." << std::endl;
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             }
-            break;
-        case 2:
-            std::cout << "Testas atsisakytas." << std::endl;
-            std::cout << "Programa baigė darbą." << std::endl;
-            break;
+            else {
+                patikrinimas2 = true;
+            }
+        }
+
+        switch(koalicijosPasirinkimas){
+            case 1:
+                {
+                int length;
+                std::cout << "Įveskite simbolių kiekį, kurį norėtumėte sugeneruoti: ";
+                std::cin >> length;
+                KoalizijosPatikra(length);
+                }
+                break;
+            case 2:
+                std::cout << "Koalizijų testas praleistas." << std::endl;
+                break;
+        }
+
+        std::cout << "Ar norėtumėte atlikti lavinos efekto testą (100k porų)? (1 - Taip, 2 - Ne): ";
+        int lavinosPasirinkimas;
+        bool patikrinimas3 = false;
+        while(!patikrinimas3){
+            std::cin >> lavinosPasirinkimas;
+            if(lavinosPasirinkimas < 1 || lavinosPasirinkimas > 2){
+                std::cout << "Klaida! Įveskite skaičių." << std::endl;
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            }
+            else {
+                patikrinimas3 = true;
+            }
+        }
+        switch(lavinosPasirinkimas){
+            case 1:
+                TestuokLavinosEfekta(HashFunkcija);
+                break;
+            case 2:
+                std::cout << "Lavinos efekto testas praleistas." << std::endl;
+                break;
+        }
+
+        std::cout << "Ar noretumete atlikti negriztamumo demonstracija? (1 - Taip, 2 - Ne): ";
+        int negriztamumoPasirinkimas;
+        bool patikrinimas4 = false;
+        while(!patikrinimas4){
+            std::cin >> negriztamumoPasirinkimas;
+            if(negriztamumoPasirinkimas < 1 || negriztamumoPasirinkimas > 2){
+                std::cout << "Klaida! Įveskite skaičių." << std::endl;
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            }
+            else {
+                patikrinimas4 = true;
+            }
+        }
+        switch(negriztamumoPasirinkimas){
+            case 1:
+                {
+                std::string inputas;
+                std::cout << "Įveskite norimą tekstą: ";
+                std::cin >> inputas;
+                TestHiding(inputas);
+                TestPuzzleFriendliness(inputas);
+                }
+                break;
+            case 2:
+                std::cout << "Negrįžtamumo testas praleistas." << std::endl;
+                break;
+        }
     }
+    
+    // Klausti ar nori kartoti programą
+    std::cout << "Ar norėtumėte kartoti programą su kitu tekstu/failu? (1 - Taip, 2 - Ne): ";
+    int kartojimosPasirinkimas;
+    bool patikrinimas5 = false;
+    while(!patikrinimas5){
+        std::cin >> kartojimosPasirinkimas;
+        if(kartojimosPasirinkimas < 1 || kartojimosPasirinkimas > 2){
+            std::cout << "Klaida! Įveskite skaičių." << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+        else {
+            patikrinimas5 = true;
+        }
+    }
+    
+    if(kartojimosPasirinkimas == 2) {
+        kartoti = false;
+        std::cout << "Programa baigė darbą." << std::endl;
+    }
+    
+    } // End of main while loop
 
     return 0;
-    // 904418064
-    // 904418064
-
 }
